4ddd7727c126de499f489a577037397fdd444820
meera <myaalshara@gmail.com>
2024-01-22 22:48:56 +0400

    first commit

---
+++ happybirthday.py
@@ -0,0 +1,7 @@
+
+name = input("enter your name: ")
+birth_month = input("enter your birth month: ")
+birth_day = input("enter your birth day: ")
+birth_year = input("enter your birth year: ")
+    
+print( name ," !", "your birthday is on " ,birth_month, birth_day, birth_year, " !")
